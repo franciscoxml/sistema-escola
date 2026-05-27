1020d2282752498d6c7d14f40a26c62d9983c881
unknown <seuemail@gmail.com>
2026-05-27 14:55:42 -0500

    Conectando frontend com API online

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const API = 'http://localhost:3001'
+const API = 'https://sistema-escola-api.onrender.com'
 
 export default function App() {
 
